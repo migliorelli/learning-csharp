e6c7134a4d0acdca5689669ace73cbb9e54475ec
Miguel Migliorelli <miglenti@gmail.com>
2024-12-11 15:53:37 -0300

    jackal chat app

---
+++ JackalChat/JackalChat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using JackalChat.Core;
+using JackalChat.MVVM.Model;
+
+namespace JackalChat.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+
+        public RelayCommand SendCommand { get; set; }
+
+
+        private ContactModel _selectedContact;
+            
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set { _selectedContact = value; OnPropertyChanged(); }
+        }
+
+
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; OnPropertyChanged(); }
+        }
+
+
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    IsFirstMessage = false
+                });
+
+                Message = "";
+            });
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                for (int j = 0; j < 4; j++)
+                {
+                    Messages.Add(new MessageModel
+                    {
+                        Username = $"Person ${i + 1}",
+                        UsernameColor = "#4098ff",
+                        ImageSource = "https://t3.ftcdn.net/jpg/08/20/75/34/360_F_820753420_Nqjb8USaj0J7K82Uo6yZLhCv4roZFBj7.jpg",
+                        Message = $"Message {j + 1}",
+                        Time = DateTime.Now,
+                        IsNativeOrigin = false,
+                        IsFirstMessage = true
+                    });
+                }
+
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Person {i + 1}",
+                    ImageSource = "https://t3.ftcdn.net/jpg/08/20/75/34/360_F_820753420_Nqjb8USaj0J7K82Uo6yZLhCv4roZFBj7.jpg",
+                    Messages = Messages
+                });
+            }
+        }
+    }
+}
---
+++ JackalChat/JackalChat/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JackalChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* we need this because only checking if the click count is two cause some bugs when dragmoving */
+        private DateTime lastClickTime = DateTime.MinValue;
+        private readonly int doubleClickThreshold = 300;
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            bool validDoubleClick = (currentTime - lastClickTime).TotalMilliseconds <= doubleClickThreshold;
+
+            if (lastClickTime != DateTime.MinValue && validDoubleClick)
+            {
+                HandleMaximize();
+            }
+            else
+            {
+                DragMove();
+            }
+
+            lastClickTime = currentTime;
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            HandleMaximize();
+        }
+
+        private void HandleMaximize()
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else WindowState = WindowState.Maximized;
+        }
+
+    }
+}
