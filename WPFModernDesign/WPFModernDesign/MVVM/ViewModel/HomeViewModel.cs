5b5e0a244c362fcdb1a1598cc9562133c75b0f59
Miguel Migliorelli <miglenti@gmail.com>
2024-12-11 12:34:21 -0300

    wpf modern design app

---
+++ WPFModernDesign/WPFModernDesign/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WPFModernDesign.MVVM.ViewModel
+{
+    internal class HomeViewModel
+    {
+    }
+}
---
+++ WPFModernDesign/WPFModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using WPFModernDesign.Core;
+
+namespace WPFModernDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private object _currentView;
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            DiscoveryVm = new DiscoveryViewModel();
+            FeaturedVm = new FeaturedViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVm;
+            });
+
+            FeaturedViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = FeaturedVm;
+            });
+
+        }
+
+        public HomeViewModel HomeVm { get; set; }
+        public DiscoveryViewModel DiscoveryVm { get; set; }
+        public FeaturedViewModel FeaturedVm { get; set; }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChange(); }
+        }
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public RelayCommand FeaturedViewCommand { get; set; }
+    }
+}
