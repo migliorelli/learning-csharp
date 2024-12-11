ef5105bc20399185e1b4b852a61bec8fdfbee430
Miguel Migliorelli <miglenti@gmail.com>
2024-12-11 00:23:51 -0300

    my first project

---
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("How old are you? ");
+            string ageStr = Console.ReadLine();
+            int age = Convert.ToInt32(ageStr);
+
+            Console.WriteLine($"Your name is {name} and you are {age} years old.");
+
+
+        }
+    }
+}
